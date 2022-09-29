1cb61e5c3c1e44619208bb22c61381974ef17b76
Yana <yanaegorova69.3@gmail.com>
2022-09-29 21:26:02 +0300

    +

--- js/local-storage.js
+++ js/local-storage.js
@@ -1,24 +1,36 @@
 import { products } from './data/products.js';
-export const localStorage = (id, count, ringSize = 6) => {
-  
+export const localStorage = (id, count) => {
     const actualProduct = products.find(product => product.id === id);
-  
-
     let cart = window.localStorage.getItem('cart');
+    // DEV
+    var taskStatus
+    if(actualProduct.isItRing){
+        if(document.querySelector(".prod__select").value == "Select"){
+            document.querySelector(".prod__select").focus()
+            taskStatus = false
+            alert("Select a ring size")
+        } else {
+            taskStatus = true
+            actualProduct.ringSize = document.querySelector(".prod__select").value
+        }
+    }
+    if(taskStatus == false){
+        return taskStatus
+    }
+    /////
     if(cart) {
         cart = JSON.parse(cart);
-        const isFindProduct = cart.some(product => product.id === id);
+    // DEV
+        var isFindProduct
+        if(actualProduct.isItRing){
+            isFindProduct = cart.some(product => product.id === id && product.ringSize == actualProduct.ringSize);
+        } else {
+            isFindProduct = cart.some(product => product.id === id);
+        }
         if(isFindProduct) {
-            const updateCart = cart.map(product => {
-               
-                if(product.id === id) {
-                    return {...product,ringSize: product.isItRing ? ringSize : null,quantity : count ? Number(count) : product.quantity + 1};
-                } else {
-                    return product;
-                }
-              
-            });
-
+            // const updateCart = [...cart, {...actualProduct, quantity : count ? Number(count) : 1}];
+            const updateCart = cart.map(product => product.id === id ? {...product, quantity : product.quantity + 1 } : product);
+    //////            
             window.localStorage.setItem('cart', JSON.stringify(updateCart));
             return;
         }
@@ -26,12 +38,9 @@ export const localStorage = (id, count, ringSize = 6) => {
         window.localStorage.setItem('cart', JSON.stringify(updateCart));
         return;
     };
-    const newProduct = {...actualProduct,  quantity : count ? Number(count) : 1};
-    if(actualProduct.isItRing) {
-        newProduct.ringSize = ringSize;
-    }
-    const createCart = [newProduct];
+    const createCart = [{...actualProduct, quantity : count ? Number(count) : 1}];
     window.localStorage.setItem('cart', JSON.stringify(createCart));
+    return taskStatus
 }
 
 export const getLocalStorageItem = () => {
